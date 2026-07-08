3deb55228b545f265df4dfac22e2da7fcaa1e14a
Anushka <anushkapandey8029@gmail.com>
2026-07-09 00:05:14 +0530

    Update 1929. Concatenation of Array.java

--- 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.java
+++ 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.java
@@ -7,3 +7,33 @@ class Solution {
         return ans;
     }
 }
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int n= nums.length;
+//         int[] ans= new int[2*n];
+//         for(int i=0 ; i < n ; i++){
+//             ans[i] = nums[i]; 
+//             ans[i + n] = nums[i];
+//         }
+//         return ans;
+        
+//     }
+// }
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for(int i = 0; i < nums.length; i++) {
+//             list.add(nums[i]);
+//         }
+//         ArrayList<Integer> l=new ArrayList<>();
+//         l.addAll(list);
+//         l.addAll(list);
+//         int[] ans = new int[l.size()];
+//         for (int i = 0; i < l.size(); i++) {
+//             ans[i] = l.get(i);
+//         }
+//         return ans;
+//     }
+// }
