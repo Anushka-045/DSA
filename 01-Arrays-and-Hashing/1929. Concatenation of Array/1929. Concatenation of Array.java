598ea70a3494de7009da23f3f1487070d880abd7
Anushka <anushkapandey8029@gmail.com>
2026-07-09 00:03:51 +0530

    [1929] [Time Beats: 100.00%] [Memory Beats: 36.29%] - Leet2Hub

--- 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.java
+++ 01-Arrays-and-Hashing/1929. Concatenation of Array/1929. Concatenation of Array.java
@@ -1,12 +1,9 @@
-1class Solution {
-2    public int[] getConcatenation(int[] nums) {
-3        int n=nums.length;
-4        int[] ans = new int[2*n];
-5        for (int i = 0; i < n; i++) {
-6             ans[i] = nums[i];
-7             ans[i + n] = nums[i];
-8
-9        }
-10        return ans;
-11    }
-12}
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        System.arraycopy(nums, 0, ans, 0, n);
+        System.arraycopy(nums, 0, ans, n, n);
+        return ans;
+    }
+}
