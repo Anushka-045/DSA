49467cc9a27ed3952bc9112791693cc9ca6001b3
Anushka <anushkapandey8029@gmail.com>
2026-07-11 01:47:21 +0530

    [217] [Time Beats: 95.83%] [Memory Beats: 19.72%] - Leet2Hub

--- 01-Arrays-and-Hashing/217. Contains Duplicate/217. Contains Duplicate.java
+++ 01-Arrays-and-Hashing/217. Contains Duplicate/217. Contains Duplicate.java
@@ -1,14 +1,38 @@
 1class Solution {
 2    public boolean containsDuplicate(int[] nums) {
-3        HashSet<Integer> seen = new HashSet<>();
-4        for(int i: nums){
-5            if(!seen.contains(i)){
-6                seen.add(i);
-7            }
-8            else{
-9                return true;
-10            }
-11        }
-12        return false;
-13    }
-14}
+3        // HashSet<Integer> l =new HashSet<>();
+4        // for (int i=0;i<nums.length;i++){
+5        //     if(l.contains(nums[i])){
+6        //         return true;
+7        //     }
+8        //     else{
+9        //         l.add(nums[i]);
+10        //     }
+11        // }
+12        // return false;   
+13
+14        // Arrays.sort(nums);
+15        // for(int i=0;i<nums.length-1;i++){
+16        //     if(nums[i]==nums[i+1]){
+17        //         return true;
+18        //     }
+19        // }
+20        // return false;
+21
+22        // HashMap<Integer, Integer> map = new HashMap<>();
+23        //for (int num : nums) {
+24        //    if (map.containsKey(num)) {
+25        //         return true;
+26        //    }
+27        //     map.put(num, 1);
+28        //}
+29        // return false;
+30       // }
+31        HashSet<Integer> set=new HashSet<>(nums.length *2);
+32        for(int num:nums){
+33            if(!set.add(num)) return true;
+34        }
+35        return false;
+36    }
+37}
+38
