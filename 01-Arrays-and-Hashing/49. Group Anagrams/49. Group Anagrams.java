5e2db01283f8cc8abf2eea99d5bb56232b080f60
Anushka <anushkapandey8029@gmail.com>
2026-07-13 01:25:36 +0530

    [49] [Time Beats: 99.51%] [Memory Beats: 72.47%] - Leet2Hub

--- 01-Arrays-and-Hashing/49. Group Anagrams/49. Group Anagrams.java
+++ 01-Arrays-and-Hashing/49. Group Anagrams/49. Group Anagrams.java
@@ -1,13 +1,17 @@
-1class Solution {
-2    public List<List<String>> groupAnagrams(String[] strs) {
-3        HashMap<String, List<String>> map = new HashMap<>();
-4        for(String s: strs){
-5            char[] c = s.toCharArray();
-6            Arrays.sort(c);
-7            String k=new String(c);
-8            map.putIfAbsent(k,new ArrayList<>());
-9            map.get(k).add(s);
-10        }
-11        return new ArrayList<>(map.values());
-12    }
-13}
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());        
+    }
+}
